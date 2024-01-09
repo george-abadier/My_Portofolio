993d4cb7e2556c719e8feece9c2a10d98af9894b
george-abadier <106331958+george-abadier@users.noreply.github.com>
2024-01-09 18:08:42 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# My_Portofolio
-My Portofolio Website
+### Responsive Portfolio Website George
+
+- Responsive Personal Portfolio Website Using HTML Bootstrap CSS & JavaScript
+- Smooth scrolling in each section.
+- Includes a light and dark mode.
+- Developed first with the Mobile First methodology, then for desktop.
+- Compatible with all mobile devices and with a beautiful and pleasant user interface.
+
+## Created By
+
+<table align="center">
+  <tr>
+    <td align="center">
+      <a href="https://github.com/george-abadier">
+        <img src="assets/img/photoCircle.jpg" width="100px" alt="My Portfolio">
+      </a>
+      <br>
+      <a href="https://github.com/george-abadier">George Abadier</a>
+    </td>
+  </tr>
+</table>
---
+++ assets/js/main.js
@@ -0,0 +1,65 @@
+// const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+// const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+/*==================== SHOW SCROLL UP ====================*/ 
+function scrollUp(){
+  const scrollUp = document.getElementById('scroll-up');
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the
+  if(this.scrollY >= 560){
+    scrollUp.classList.remove('d-none');
+    scrollUp.classList.add('d-block');
+  }  
+  else {
+    scrollUp.classList.remove('d-block');
+    scrollUp.classList.add('d-none');
+  }
+}
+window.addEventListener('scroll', scrollUp)
+
+/*==================== ANIMATION FOR TEXT TITLE ====================*/ 
+const animatedText = document.getElementById('animatedText');
+const dynamicText = document.getElementById('dynamicText');
+const textArray = [
+    'web applications using React & NodeJS.',
+    'web applications using Angular & C#.',
+    'web interface.',
+    'automation tools.'
+
+];
+let currentIndex = 0;
+
+function animateText() {
+    const currentText = textArray[currentIndex];
+    const chars = currentText.split('');
+
+    let animation = setInterval(() => {
+    if (chars.length > 0) {
+        dynamicText.innerHTML += chars.shift();
+    } else {
+        clearInterval(animation);
+        setTimeout(() => {
+        eraseText();
+        }, 1000);
+    }
+    }, 50);
+}
+
+function eraseText() {
+    let chars = dynamicText.innerHTML.split('');
+    let animation = setInterval(() => {
+    if (chars.length > 0) {
+        chars.pop();
+        dynamicText.innerHTML = chars.join('');
+    } else {
+        clearInterval(animation);
+        currentIndex = (currentIndex + 1) % textArray.length;
+        setTimeout(() => {
+        dynamicText.innerHTML = ''; // Clear dynamic text
+        animateText();
+        }, 500);
+    }
+    }, 50);
+}
+
+// Start the initial animation
+animateText();
